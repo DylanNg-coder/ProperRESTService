0956306cf0c723a10eb249b325210d71db776d40
DylanNg-coder <p3rs0nal1tY>
2021-02-12 10:38:21 +0000

    first commit

---
+++ src/main/java/com/omanda/ProperRestService/ProperRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omanda.ProperRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProperRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProperRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omanda/ProperRestService/ProperRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omanda.ProperRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProperRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
