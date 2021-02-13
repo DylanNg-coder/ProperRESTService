46f53066675de4c1bcef3c8e8cc170089ca8f2c2
DylanNg-coder <p3rs0nal1tY>
2021-02-13 13:12:17 +0000

    Add preloaded values to database and allows access through JPA

---
+++ src/main/java/com/omanda/ProperRestService/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.omanda.ProperRestService;
+
+import com.omanda.ProperRestService.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
